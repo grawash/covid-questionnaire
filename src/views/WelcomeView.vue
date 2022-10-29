30289dd8ee18e14d631d441c15f8e7abf4baccd7
zurbash <duta1101@bk.ru>
2022-10-29 17:47:32 +0400

    feat: added welcome page

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,11 +1,14 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <div class="flex h-screen">
     <div class="flex flex-col ml-auto mr-auto mt-auto mb-auto">
       <img src="../assets/Vector(1).svg" alt="Rocketman" />
-      <router-link to="/personal-info" class="mt-28">yooo</router-link>
+      <router-link
+        to="/personal-info"
+        class="mt-28 font-extrabold text-3xl text-center wrap hover:text-shadow-black"
+        >კითხვარის <br> დაწყება</router-link
+      >
     </div>
   </div>
 </template>
