c1eea65db9139f31563a88064ee86dff7da51c2e
zurbash <duta1101@bk.ru>
2022-10-29 17:10:08 +0400

    feat: added tailwind

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,5 +1,11 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <div></div>
+  <div class="flex h-screen">
+    <div class="flex flex-col ml-auto mr-auto mt-auto mb-auto">
+      <img src="../assets/Vector(1).svg" alt="Rocketman" />
+      <router-link to="/personal-info" class="mt-28">yooo</router-link>
+    </div>
+  </div>
 </template>
